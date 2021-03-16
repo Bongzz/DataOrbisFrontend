b3121fe3bd2f83afea676c52ed2cecbd3551a32f
bongz <mbongisenimncube2@gmail.com>
2021-03-16 17:06:13 +0200

    imported httpClient  module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { LayoutContainerComponent } from './components/layout-container/layout-container.component';
 import { MatMenuModule} from '@angular/material/menu';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
 import { ProductlistComponent } from './components/productlist/productlist.component';
 
 @NgModule({
@@ -17,7 +19,8 @@ import { ProductlistComponent } from './components/productlist/productlist.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
